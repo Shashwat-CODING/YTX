1d26e0d6aba5d2c56681f32e3215607cde42669c
Shashwat-CODING <bob17040246@gmail.com>
2025-11-25 22:58:44 +0530

    Clean

--- lib/app_config.dart
+++
@@ -1,9 +0,0 @@
-AppConfig appConfig = AppConfig(version: 38, codeName: '2.0.13');
-
-class AppConfig {
-  int version;
-  String codeName;
-  Uri updateUri = Uri.parse(
-      'https://api.github.com/repos/jhelumcorp/Vel_MuSic/releases/latest');
-  AppConfig({required this.version, required this.codeName});
-}
--- lib/ytmusic/mixins/playlists.dart
+++
@@ -1,3 +0,0 @@
-import 'package:vel_music/ytmusic/yt_service_provider.dart';
-
-mixin PlaylistsMixin on YTMusicServices{}
